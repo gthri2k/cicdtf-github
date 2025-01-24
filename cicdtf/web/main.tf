0ecdaaf4cb4e7d2b1b3485a65027a9c7f797feff
gthri_2k <gthri.tech@gmail.com>
2025-01-24 09:54:50 +0800

    message for main.tf

--- cicdtf/web/main.tf
+++ cicdtf/web/main.tf
@@ -1,12 +1,12 @@
 #ec2
 
-resource "aws_instance" "servernew" {
+resource "aws_instance" "servernew-gaya" {
   ami = "ami-05576a079321f21f8"
   instance_type = "t2.micro"
   subnet_id = var.sn
   security_groups = [var.sg]
 
   tags = {
-    Name = "myserver"
+    Name = "myserver-gaya"
   }
 }
